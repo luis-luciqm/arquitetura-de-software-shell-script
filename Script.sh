b8c71ee82ad0173eae4bb7a8af5a8414a3321b81
luis-luciqm <redbullfc18@gmail.com>
2021-06-18 20:54:33 -0300

    first commit - Shell Script

--- Script.sh
+++ Script.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
-    n1=0
-    n2=0
+n1=0
+n2=0
 
-    echo "Bem vindo à selva"
-    echo "------------------"
+echo "Bem vindo à selva"
+echo "------------------"
 
-    echo "Digite o primeiro número: "
-        read n1
-    echo "Digite o segundo número: "
-        read n2
+echo "Digite o primeiro número: "
+    read n1
+echo "Digite o segundo número: "
+    read n2
 
-    echo "---------------"
-    echo "Escolha qual das opções deseja!"
-    echo "1 - Resultado da Soma"
-    echo "2 - Resultado da Multiplicação"
-    echo "3 - Resultado da Subtração"
-    echo "4 - Resultado da Divisão"
-    read op
+echo "---------------"
+echo "Escolha qual das opções deseja!"
+echo "1 - Resultado da Soma"
+echo "2 - Resultado da Multiplicação"
+echo "3 - Resultado da Subtração"
+echo "4 - Resultado da Divisão"
+read op
 
-    if [ $op -eq 1 ]
+if [ $op -eq 1 ]
+then
+    echo "Resultado: $[n1+n2]"
+fi
+
+if [ $op -eq 2 ] 
+then
+    echo "Resultado: $[n1*n2]"
+fi
+
+if [ $op -eq 3 ] 
+then
+    echo "Resultado: $[n1-n2]"
+fi
+
+if [ $op -eq 4 ] 
+then
+    if [ $n2 != 0 ] 
     then
-        echo "Resultado: $[n1+n2]"
+        echo "Resultado: $[n1/n2]"
+    else
+        echo "Impossivel Dividir por Zero!"
     fi
+fi
 
-    if [ $op -eq 2 ] 
-    then
-        echo "Resultado: $[n1*n2]"
-    fi
-
-    if [ $op -eq 3 ] 
-    then
-        echo "Resultado: $[n1-n2]"
-    fi
-
-    if [ $op -eq 4 ] 
-    then
-        if [ $n2 != 0 ] 
-        then
-            echo "Resultado: $[n1/n2]"
-        else
-            echo "Impossivel Dividir por Zero!"
-        fi
-    fi
